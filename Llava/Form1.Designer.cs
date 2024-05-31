bae66b362dc0eb9ecb8d1d762e6f25007d9939a0
enjoyst1ck <bwwojcik0@gmail.com>
2024-05-31 18:51:17 +0200

    Zadanie z AI - llava

---
+++ Llava/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Llava
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBoxQuestion = new TextBox();
+            textBoxAnswer = new TextBox();
+            pictureBox1 = new PictureBox();
+            buttonWoP = new Button();
+            buttonClear = new Button();
+            buttonSend = new Button();
+            buttonLoadPicture = new Button();
+            buttonPaste = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBoxQuestion
+            // 
+            textBoxQuestion.Location = new Point(12, 12);
+            textBoxQuestion.Multiline = true;
+            textBoxQuestion.Name = "textBoxQuestion";
+            textBoxQuestion.Size = new Size(313, 140);
+            textBoxQuestion.TabIndex = 0;
+            // 
+            // textBoxAnswer
+            // 
+            textBoxAnswer.Location = new Point(12, 169);
+            textBoxAnswer.Multiline = true;
+            textBoxAnswer.Name = "textBoxAnswer";
+            textBoxAnswer.Size = new Size(313, 140);
+            textBoxAnswer.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(344, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(444, 426);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // buttonWoP
+            // 
+            buttonWoP.Location = new Point(12, 328);
+            buttonWoP.Name = "buttonWoP";
+            buttonWoP.Size = new Size(166, 23);
+            buttonWoP.TabIndex = 3;
+            buttonWoP.Text = "Zapytaj co jest na obrazku?";
+            buttonWoP.UseVisualStyleBackColor = true;
+            buttonWoP.Click += buttonWoP_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(12, 368);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(166, 23);
+            buttonClear.TabIndex = 4;
+            buttonClear.Text = "Wyczyść";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // buttonSend
+            // 
+            buttonSend.Location = new Point(12, 406);
+            buttonSend.Name = "buttonSend";
+            buttonSend.Size = new Size(166, 23);
+            buttonSend.TabIndex = 5;
+            buttonSend.Text = "Wyślij";
+            buttonSend.UseVisualStyleBackColor = true;
+            buttonSend.Click += buttonSend_Click;
+            // 
+            // buttonLoadPicture
+            // 
+            buttonLoadPicture.Location = new Point(184, 328);
+            buttonLoadPicture.Name = "buttonLoadPicture";
+            buttonLoadPicture.Size = new Size(141, 23);
+            buttonLoadPicture.TabIndex = 6;
+            buttonLoadPicture.Text = "Wczytaj obrazek";
+            buttonLoadPicture.UseVisualStyleBackColor = true;
+            buttonLoadPicture.Click += buttonLoadPicture_Click;
+            // 
+            // buttonPaste
+            // 
+            buttonPaste.Location = new Point(184, 368);
+            buttonPaste.Name = "buttonPaste";
+            buttonPaste.Size = new Size(141, 23);
+            buttonPaste.TabIndex = 7;
+            buttonPaste.Text = "Wklej";
+            buttonPaste.UseVisualStyleBackColor = true;
+            buttonPaste.Click += buttonPaste_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonPaste);
+            Controls.Add(buttonLoadPicture);
+            Controls.Add(buttonSend);
+            Controls.Add(buttonClear);
+            Controls.Add(buttonWoP);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBoxAnswer);
+            Controls.Add(textBoxQuestion);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxQuestion;
+        private TextBox textBoxAnswer;
+        private PictureBox pictureBox1;
+        private Button buttonWoP;
+        private Button buttonClear;
+        private Button buttonSend;
+        private Button buttonLoadPicture;
+        private Button buttonPaste;
+    }
+}
---
+++ Llava/Form1.cs
@@ -0,0 +1,123 @@
+using Newtonsoft.Json.Linq;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace Llava
+{
+    public partial class Form1 : Form
+    {
+        private string conversationHistory = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonWoP_Click(object sender, EventArgs e)
+        {
+            textBoxQuestion.Text = "What's in this image?";
+            AskOllama();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            conversationHistory = "";
+            textBoxQuestion.Text = "";
+            textBoxAnswer.Text = "";
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            AskOllama();
+        }
+
+        private void buttonLoadPicture_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files(.bmp;.jpg; .jpeg;.png)| .bmp;.jpg; .jpeg;.png";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = openFileDialog.FileName;
+                    pictureBox1.Image = new Bitmap(fileName);
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using (FileStream fs = new FileStream(fileName, FileMode.Open,
+                       FileAccess.Read))
+                        {
+                            fs.CopyTo(memoryStream);
+                        }
+                        byte[] imageBytes = memoryStream.ToArray();
+                        pictureBox1.Tag = Convert.ToBase64String(imageBytes);
+                    }
+                }
+            }
+        }
+
+        private void buttonPaste_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+                Image image = Clipboard.GetImage();
+                pictureBox1.Image = image;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    image.Save(ms, ImageFormat.Png);
+                    byte[] imageBytes = ms.ToArray();
+                    string base64Image = Convert.ToBase64String(imageBytes);
+                    pictureBox1.Tag = base64Image;
+                }
+            }
+        }
+
+        private async void AskOllama()
+        {
+            if (!string.IsNullOrEmpty(textBoxQuestion.Text))
+                textBoxAnswer.Text = "Czekaj na odpowiedü...";
+            else return;
+            try
+            {
+                var client = new HttpClient();
+                client.Timeout = TimeSpan.FromMinutes(10);
+                using (client)
+                {
+                    var json = Newtonsoft.Json.JsonConvert.SerializeObject(new
+                    {
+                        model = "llava",
+                        prompt = conversationHistory + textBoxQuestion.Text,
+                        images = new JArray(pictureBox1.Tag)
+                    });
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+                    var response = await client.PostAsync("http://localhost:11434/api/generate", content);
+
+                    if (!response.IsSuccessStatusCode) throw new Exception($"Error: {response.StatusCode}");
+
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    string textOfModelAnswer = string.Join(",", responseBody.Split("\n".ToCharArray()));
+                    if (textOfModelAnswer.EndsWith(",")) textOfModelAnswer = textOfModelAnswer.Substring(0, textOfModelAnswer.Length - 1);
+                    textOfModelAnswer = $"[{textOfModelAnswer}]";
+
+                    JArray jsonResponse = JArray.Parse(textOfModelAnswer);
+                    textOfModelAnswer = "";
+
+                    for (int i = 0; i < jsonResponse.Count; ++i)
+                        textOfModelAnswer += jsonResponse[i]["response"].ToString();
+                    if (!string.IsNullOrEmpty(textOfModelAnswer))
+                    {
+                        textBoxAnswer.Text = textBoxQuestion.Text + " \r\n\r\n";
+                        textBoxAnswer.Text += textOfModelAnswer.Replace("\n", "\r\n").Trim();
+                        textBoxQuestion.Text = "";
+                        conversationHistory += textBoxAnswer.Text + " \r\n\r\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                textBoxAnswer.Text = $"{ex.Message}";
+            }
+        }
+
+    }
+}
